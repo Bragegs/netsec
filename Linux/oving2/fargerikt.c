6476ea2ad9e7fcc30cc7882f99d7ba566420c910
Bragegs <bragestaven94@hotmail.com>
2016-09-19 20:50:50 +0200

    Forste commit

---
+++ Linux/oving2/fargerikt.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "fargeskrift.h"
+
+int main(int argc, char *argv[]) {
+	farge_printf(2, 1, "Du ", 2);
+        farge_printf(3, 2, "kjørte ", 3);
+        farge_printf(4, 3, "programmet", 4);
+        farge_printf(5, 0, " fargerikt.c", 1);
+
+
+}
