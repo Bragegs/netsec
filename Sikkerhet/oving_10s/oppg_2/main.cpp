cfab289f2cd5923e830d3289104eca3bf6368bdf
Brage G. Staven <bragestaven94@hotmail.com>
2016-09-20 14:25:07 +0200

    dekryptert

--- Sikkerhet/oving_10s/oppg_2/main.cpp
+++ Sikkerhet/oving_10s/oppg_2/main.cpp
@@ -20,20 +20,31 @@ int main() {
   //Choice of distribution with 1 byte values
   uniform_int_distribution<char> distribution;
   string kryptert_melding = "114b70745a521c57371f7a245d6440662d49";
-  string tekst = "";
+  //string tekst = "";
+  string out;
+  int counter = 0;
     //Retrieve random numbers from the generator using the chosen distribution:
   for (size_t c = 0; c < 5; ++c) {
+    string temp = "0x";
+    temp += kryptert_melding[i];
+    temp += kryptert_melding[i+1];
+    
+    int hexpart = stoul(temp, nullptr, 16);
+    
+    out.insert(counter,1,hexpart^(int)distribution(generator));
+
     //cout << (int)distribution(generator) << endl;
    // cout << (char)((int)distribution(generator)^kryptert_melding.at(c)) << endl;
     // cout << (char)distribution(generator) << endl;
     int x = stoul(kryptert_melding, nullptr, 16);
     
-    cout << x^(int)distribution(generator) << endl;
+    //cout << x^(int)distribution(generator) << endl;
     // char e = (char)distribution(generator);
     //tekst += e;
+    counter++; 
   }
 
-  
+  cout << out << endl;
 
   
   
