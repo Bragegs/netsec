a346837c1260f49c3568bf313d7e5ea9e7b59cab
Bragegs <bragestaven94@hotmail.com>
2016-10-03 20:37:14 +0200

    oving_14s start

---
+++ Sikkerhet/oving_14s/config/passport.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.serializeUser(function(user, done) {
+  done(null, "123456");
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, "123456");
+});
+
+passport.use(new GoogleStrategy({
+        clientID: '812454777427-47jjg72uekfhgdpq34s5mrdcbequqeie.apps.googleusercontent.com',//process.env.GOOGLE_CLIENT_ID,
+        clientSecret: 'DREohO7If_fWA2annUMVY2GD',//process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL:  '/auth/google/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function () {
+            return done(null, profile);
+        });
+    }
+));
+//GOOGLE_ID=812454777427-47jjg72uekfhgdpq34s5mrdcbequqeie.apps.googleusercontent.com
+//GOOGLE_SECRET=DREohO7If_fWA2annUMVY2GD
